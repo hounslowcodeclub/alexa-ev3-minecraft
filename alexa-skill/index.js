ebd2486fad1ddb172c27bc01acd025d852d7db95
hounslowcodeclub <hounslowcodeclub@gmail.com>
2018-05-04 11:49:28 +0100

    Update index.js

--- alexa-skill/index.js
+++ alexa-skill/index.js
@@ -105,7 +105,7 @@ const handlers = {
 
 exports.handler = function (event, context, callback) {
     const alexa = Alexa.handler(event, context, callback);
-    alexa.APP_ID = APP_ID;
+    alexa.appId = APP_ID;
     alexa.resources = languageStrings;
     alexa.registerHandlers(handlers);
     alexa.execute();
